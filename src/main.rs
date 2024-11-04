62e64872807dd408a128b2985558960556e13de3
Barboutchou <samuel.godlewski-loyer@dauphine.eu>
2024-11-04 18:31:32 +0100

    Impl de liste chaînées laboriseuses

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+pub mod first;
+use first::List;
+
+// use nalgebra::{Matrix2, Vector3};
+
+/* fn main() {
+    let matrix_of_vectors = Matrix2::new(
+        Vector3::new(1.0, 2.0, 3.0),
+        Vector3::new(4.0, 5.0, 6.0),
+        Vector3::new(7.0, 8.0, 9.0),
+        Vector3::new(10.0, 11.0, 12.0),
+    );
+    let vector = matrix_of_vectors[(0, 1)];
+    println!("Vecteur en (0, 1) : {:?}", vector);
+} */
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
